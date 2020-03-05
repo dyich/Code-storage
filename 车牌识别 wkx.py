06bfa38e4f194efd6d15533d59380a65cfc5275a
ShineYC <51308621+ShineYC@users.noreply.github.com>
2020-03-05 23:01:06 +0800

    Update 车牌识别 wkx.py

--- 车牌识别 wkx.py
+++ 车牌识别 wkx.py
@@ -2,14 +2,18 @@
 
 import requests
 import base64
-
+import urllib.request
+import os ,stat
 '''
 车牌识别
 '''
 
+url = "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2653272359,224531920&fm=26&gp=0.jpg"
+urllib.request.urlretrieve(url,filename="Desktop/new.jpg")
+
 request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/license_plate"
 # 二进制方式打开图片文件
-f = open('Desktop/ling.jpg','rb')
+f = open('Desktop/new.jpg','rb')
 img = base64.b64encode(f.read())
 params = {"image":img}
 access_token = '24.0792a5692746cd91785d80a7885a7a95.2592000.1585822854.282335-18659015'
